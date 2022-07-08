2f6d89fd59e5e850168f04411192a7d1680f5b71
SAM <samwfelice@gmail.com>
2022-07-08 18:28:57 +0530

    Complete todolist functions

--- src/index.js
+++ src/index.js
@@ -6,8 +6,16 @@ import { v4 as uuidv4 } from 'uuid';
 
 const inputEnter = document.querySelector('.task-input');
 const enterBtn = document.querySelector('.enter-icon');
+const clearBtn = document.querySelector('.clearBtn');
 
 document.querySelector('.refresh-icon').setAttribute('src', refresh);
+
+clearBtn.addEventListener('click', () => {
+  const selectCompleted = document.querySelectorAll('.completed');
+  selectCompleted.forEach((each) => each.remove());
+  Task.clearAllCompleted();
+  localStorage.setItem('tasks', JSON.stringify(Task.taskArr));
+});
 
 inputEnter.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
@@ -25,63 +33,11 @@ inputEnter.addEventListener('keypress', (e) => {
 enterBtn.addEventListener('click', (e) => {
   e.preventDefault();
   if (inputEnter.value) {
-    const taskItem = new Task(uuidv4(), inputEnter.value, Task.taskArr.length, false);
+    const taskItem = new Task(uuidv4(), inputEnter.value, Task.taskArr.length + 1, false);
     Task.taskArr.push(taskItem);
     taskItem.add();
     inputEnter.value = '';
+    localStorage.setItem('tasks', JSON.stringify(Task.taskArr));
   }
 });
 
-const tasks = [
-  {
-    description: 'Workout at evening',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'wash the dishes',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Make protein shake',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Meet Bill Gates',
-    completed: false,
-    index: 3,
-  },
-];
-
-tasks.sort((a, b) => a.index - b.index);
-
-const listLoad = () => {
-  tasks.forEach((each) => {
-    const taskList = document.createElement('li');
-    taskList.className = 'list';
-    const taskLabel = document.createElement('label');
-    taskLabel.className = 'list-label';
-    taskLabel.setAttribute('for', each.index);
-    const checkDescContainer = document.createElement('div');
-    checkDescContainer.className = 'description-container';
-    const taskCheck = document.createElement('input');
-    taskCheck.setAttribute('type', 'checkbox');
-    taskCheck.className = 'checkbox';
-    checkDescContainer.appendChild(taskCheck);
-    const taskDescription = document.createElement('p');
-    taskDescription.className = 'para';
-    taskDescription.textContent = each.description;
-    checkDescContainer.appendChild(taskDescription);
-    taskLabel.appendChild(checkDescContainer);
-    const moveIcon = document.createElement('img');
-    moveIcon.className = 'move-icon';
-    moveIcon.src = setting;
-    taskLabel.appendChild(moveIcon);
-    taskList.appendChild(taskLabel);
-    document.querySelector('.list-container').appendChild(taskList);
-  });
-};
-
-// listLoad();
--- src/modules/taskAdd.js
+++ src/modules/taskAdd.js
@@ -1,4 +1,5 @@
 import setting from '../assets/setting.jpg';
+import dustbin from '../assets/dustbin.jpg';
 
 
 class Task {
@@ -10,6 +11,10 @@ class Task {
   }
 
   static taskArr = [];
+
+  toggleCompleted() {
+    this.completed = !this.completed;
+  }
 
   add() {
     const taskList = document.createElement('li');
@@ -23,36 +28,94 @@ class Task {
     const taskCheck = document.createElement('input');
     taskCheck.setAttribute('type', 'checkbox');
     taskCheck.className = 'checkbox';
+    if (this.completed) {
+      taskList.classList.add('completed');
+      taskCheck.setAttribute('checked', 'true');
+    }
     taskCheck.addEventListener('change', (e) => {
       Task.markCompleted(e);
+      moveIcon.classList.toggle('d-none');
+      deleteIcon.classList.toggle('show-delete-icon');
+      const selected = Task.taskArr.filter((each) => e.target.parentElement.parentElement.parentElement.id === each.id)[0];
+      selected.toggleCompleted();
+      localStorage.setItem('tasks', JSON.stringify(Task.taskArr));
     });
     checkDescContainer.appendChild(taskCheck);
-    const taskDescription = document.createElement('p');
+    const taskDescription = document.createElement('input');
     taskDescription.className = 'para';
     taskDescription.textContent = this.description;
-    taskDescription.setAttribute('contentEditable', 'true');
+    taskDescription.setAttribute('type', 'text');
+    taskDescription.setAttribute('value', this.description);
+    taskDescription.setAttribute('disabled', 'true');
+    taskDescription.addEventListener('input', (e) => {
+      const selected = Task.taskArr.filter((each) => e.target.parentElement.parentElement.parentElement.id === each.id)[0];
+      selected.editDescription(e.target.value);
+      localStorage.setItem('tasks', JSON.stringify(Task.taskArr));
+    })
+    taskDescription.addEventListener('click', () => {
+      moveIcon.classList.toggle('d-none');
+      deleteIcon.classList.toggle('show-delete-icon');
+    });
+    checkDescContainer.addEventListener('click', () => {
+      taskDescription.removeAttribute('disabled');
+      taskDescription.setAttribute('enabled', 'true');
+    });
+    taskDescription.addEventListener('blur', () => {
+      taskDescription.removeAttribute('enabled');
+      taskDescription.setAttribute('disabled', 'true');
+      moveIcon.classList.toggle('d-none');
+      deleteIcon.classList.toggle('show-delete-icon');
+    });
     checkDescContainer.appendChild(taskDescription);
     taskLabel.appendChild(checkDescContainer);
-    const moveIcon = document.createElement('img');
+    const deleteIcon = document.createElement('i')
+    deleteIcon.className = 'delete-icon';
+    deleteIcon.addEventListener('click', (e) => {
+      Task.remove(e);
+      Task.reIndex();
+    })
+    taskLabel.appendChild(deleteIcon);
+    const moveIcon = document.createElement('i');
     moveIcon.className = 'move-icon';
-    moveIcon.src = setting;
     taskLabel.appendChild(moveIcon);
     taskList.appendChild(taskLabel);
     document.querySelector('.list-container').appendChild(taskList);
   }
-  
+
   static markCompleted(e) {
     e.target.parentElement.parentElement.parentElement.classList.toggle('completed');
-   //Add some style! Change array completed to true
+    //Add some style! Change array completed to true
   }
 
+  static remove(e) {
+    Task.taskArr = Task.taskArr.filter((each) => e.target.parentElement.parentElement.id !== each.id);
+    e.target.parentElement.parentElement.remove();
+  }
+
+  static reIndex() {
+    const newTask = [];
+    for (let i = 0; i < Task.taskArr.length; i += 1) {
+      newTask.push({ ...Task.taskArr[i], index: i + 1 })
+    }
+    Task.taskArr = newTask;
+    localStorage.setItem('tasks', JSON.stringify(Task.taskArr));
+  }
+
+  editDescription(value) {
+    this.description = value;
+  }
+
+  static clearAllCompleted() {
+    Task.taskArr =  Task.taskArr.filter((each) => !each.completed);
+  }
 }
 
 if ('tasks' in localStorage) {
-  Task.taskArr = JSON.parse(localStorage.tasks);
-  Task.taskArr.forEach((each) => {
+  const localData = JSON.parse(localStorage.tasks);
+  localData.forEach((each) => {
     const localTask = new Task(each.id, each.description, each.index, each.completed);
     localTask.add();
+    Task.taskArr.push(localTask);
   })
 }
 
