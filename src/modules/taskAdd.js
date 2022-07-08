970689fe1583637c90a9294d47e2032f118441f3
SAM <samwfelice@gmail.com>
2022-07-08 20:32:05 +0530

    fix linter errors

--- src/modules/taskAdd.js
+++ src/modules/taskAdd.js
@@ -114,8 +114,7 @@ if ('tasks' in localStorage) {
     const localTask = new Task(each.id, each.description, each.index, each.completed);
     localTask.add();
     Task.taskArr.push(localTask);
-  })
+  });
 }
-
 
 export default Task;
